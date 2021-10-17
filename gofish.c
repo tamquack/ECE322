b06958a604d00b964f45d51f73d91ad0d041b618
justinnlam <92501181+justinnlam@users.noreply.github.com>
2021-10-17 14:27:56 -0400

    Update gofish.c

--- gofish.c
+++ gofish.c
@@ -22,103 +22,103 @@ int main(int args, char* argv[]) {
      	 book(&computer);
 
 	 //if (1==1)
-	 if (game_over(&user) == 1 || game_over(&computer) == 1)
+	 	if (game_over(&user) == 1 || game_over(&computer) == 1)
+			{
+			break;
+			}
+		if (turncounter % 2 == 1) // PLAYER TURN
 		{
-		break;
+			printf("Player 1's turnAAA, enter a Rank: \n");
+			scanf("%c",GuessRank); // Change these 2 lines to user_play
+			if (search(&computer,GuessRank) == 0)// Guess = 1 if found
+			//if (Guess == 0)
+			{
+				printf("   - Player 2 has no %cs\n",GuessRank);
+				fish = deck_instance.list[deck_instance.top_card];
+				add_card(&user,next_card());
+				printf("   - Go Fish, Player 1 draws %c%c%c\n",fish.rank[0],fish.rank[1],fish.suit);			   
+				bookcheck = check_add_book(&user);
+
+				if (bookcheck != '\0')
+				{
+						printf("   - Player 1 books %c\n  - Player 1 gets another turn\n", bookcheck);
+				} 
+				
+				else
+					{
+						printf("   - Player 2's turn\n");
+						turncounter++;
+					}			
+			}
+
+			else
+			{
+			//DispResult(GuessRank); 
+				transfer_cards(&computer,&user,GuessRank);
+				check_add_book(&user);
+				if (bookcheck != '\0')
+				{
+				printf("   - Player 1 books %c\n", bookcheck);
+				}
+				printf("   - Player 1 gets another turn\n");
+				
+			}
+		} // end of player turn
+
+
+		else
+		{ // COMPUTER TURN
+			GuessR = computer_play(&user);
+			printf("Player 2's turn, enter a Rank: \n");
+			if (Guess == 0)
+			{
+				printf("   - Player 1 has no %cs\n",GuessR);
+			fish = deck_instance.list[deck_instance.top_card];
+			add_card(&computer,next_card());
+			printf("   - Go Fish, Player 2 draws %c%c%c\n",fish.rank[0],fish.rank[1],fish.suit);			   
+			bookcheck = check_add_book(&computer);
+
+			if (bookcheck != '\0')
+				{
+					printf("   - Player 2 books %c\n  - Player 2 gets another turn\n", bookcheck);
+				}
+			else{
+				printf("   - Player 1's turnBBB\n");
+				turncounter++;
+
+				}
+			
+			
+			}
+
+				else
+			{
+					//DispResult(GuessR); 
+				transfer_cards(&user,&computer,GuessR);
+				check_add_book(&computer);
+				if (bookcheck == 1)
+					{
+					printf("   - Player 2 books %c\n  - Player 2 gets another turn\n", bookcheck); 
+					}
+				else{
+				printf("   - Player 1's turnCCC\n");
+				turncounter++;
+
+				}
+			}
 		}
-         if (turncounter % 2 == 1) // PLAYER TURN
-         {
-	    printf("Player 1's turn, enter a Rank: \n");
-	    scanf("%c",GuessRank); // Change these 2 lines to user_play
-	    char Guess = search(&computer,GuessRank);// Guess = 1 if found
-	      if (Guess == 0)
-
-		 {
-	           printf("   - Player 2 has no %cs\n",GuessRank);
-		   fish = deck_instance.list[deck_instance.top_card];
-		   add_card(&user,next_card());
-		   printf("   - Go Fish, Player 1 draws %c%c%c\n",fish.rank[0],fish.rank[1],fish.suit);			   bookcheck = check_add_book(&user);
-
-		   if (bookcheck != 0)
-		       {
-		        printf("   - Player 1 books %c\n  - Player 1 gets another turn\n", bookcheck);
-		       }
-		   else{
-			printf("   - Player 2's turn\n");
-			turncounter++;
-
-			}
-		   
-		
-	  	  }
-
-	      else
-		  {
-	           //DispResult(GuessRank); 
-		   transfer_cards(&computer,&user,GuessRank);
-		   check_add_book(&user);
-		   if (bookcheck == 1)
-		       {
-		        printf("   - Player 1 books %c\n  - Player 1 gets another turn\n", bookcheck);
-		       }
-		   else{
-			printf("   - Player 2's turn\n");
-			turncounter++;
-
-			}
-		  }
-	  }
 
 
-	 else{ // COMPUTER TURN
-		GuessR = computer_play(&user);
-		printf("Player 2's turn, enter a Rank: \n");
-		if (Guess == 0)
-		 {
-	           printf("   - Player 1 has no %cs\n",GuessR);
-		   fish = deck_instance.list[deck_instance.top_card];
-		   add_card(&computer,next_card());
-		   printf("   - Go Fish, Player 2 draws %c%c%c\n",fish.rank[0],fish.rank[1],fish.suit);			   bookcheck = check_add_book(&computer);
+			
 
-		   if (bookcheck != 0)
-		       {
-		        printf("   - Player 2 books %c\n  - Player 2 gets another turn\n", bookcheck);
-		       }
-		   else{
-			printf("   - Player 1's turn\n");
-			turncounter++;
-
-			}
-		   
-		
-	  	  }
-
-	        else
-		   {
-	            //DispResult(GuessR); 
-		    transfer_cards(&user,&computer,GuessR);
-		    check_add_book(&computer);
-		    if (bookcheck == 1)
-		        {
-		         printf("   - Player 2 books %c\n  - Player 2 gets another turn\n", bookcheck); 
-		        }
-		    else{
-			 printf("   - Player 1's turn\n");
-			 turncounter++;
-
-			}
-		  }
-	  }
-
-
-	      
-
-    } // close the while loop
-    if (game_over(&computer) == 1){
+	} // close the while loop
+	if (game_over(&computer) == 1)
+	{
 	winner = 2;
 	}
-    else{
-	 winner = 1;
+	else
+	{
+	winner = 1;
 	}
 
 	printf("Player %d Wins! %d-%d\nDo you want to play again[Y/N]:\n",winner,7,6);
@@ -137,3 +137,4 @@ void book(struct player* target)
 }
 
 void DispResult(){}
+
