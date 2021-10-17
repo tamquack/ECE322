f5d6c3c4194c929b8374935528cf261ff2d467f1
tamquack <89828454+tamquack@users.noreply.github.com>
2021-10-17 15:30:21 -0400

    Add files via upload

--- gofish.c
+++ gofish.c
@@ -12,7 +12,10 @@ int main(int args, char* argv[]) {
      char YorN[1] ;
      char replay[20] ;
      struct card fish;
-     shuffle();
+	 shuffle();
+	 do{
+	 reset_player(&user);
+	 reset_player(&computer); 
      deal_player_cards(&user);
      deal_player_cards(&computer);
      
@@ -28,12 +31,11 @@ int main(int args, char* argv[]) {
 			}
 		if (turncounter % 2 == 1) // PLAYER TURN
 		{
-			printf("Player 1's turnAAA, enter a Rank: \n");
-			scanf(" %c",GuessRank); // Change these 2 lines to user_play
+			user_play(&user); 
 			if (search(&computer,GuessRank) == 0)// Guess = 1 if found
 			//if (Guess == 0)
 			{
-				printf("   - Player 2 has no %cs\n",GuessRank);
+				printf("   - Player 2 has no %ss\n",GuessRank);
 				fish = deck_instance.list[deck_instance.top_card];
 				add_card(&user,next_card());
 				printf("   - Go Fish, Player 1 draws %c%c%c\n",fish.rank[0],fish.rank[1],fish.suit);			   
@@ -95,8 +97,8 @@ int main(int args, char* argv[]) {
 			{
 					//DispResult(GuessR); 
 				transfer_cards(&user,&computer,GuessR);
-				check_add_book(&computer);
-				if (bookcheck == 1)
+				 
+				if (check_add_book(&computer))
 					{
 					printf("   - Player 2 books %c\n  - Player 2 gets another turn\n", bookcheck); 
 					}
@@ -122,19 +124,49 @@ int main(int args, char* argv[]) {
 	}
 
 	printf("Player %d Wins! %d-%d\nDo you want to play again[Y/N]:\n",winner,7,6);
-	scanf("%c",replay);
-      return 0;
+	scanf("%s",replay);
+	while(replay != 'Y' || replay != 'N'){
+		if (replay == 'N'){
+			break; 
+		}
+		else if(replay == 'Y'){
+			shuffle();
+			break;
+		}
+		else{
+			printf("Please enter Y/N:\n");
+
+		}
+	}
+if (replay == 'N'){
+	break; 
+} 
     
+}while(1);
+
 }
 
-
-void hand(struct player* target)
-{
+void hand(struct player* target){
+	struct hand* temp = target->card_list; 
+	printf("\nPlayer 1's Hand:");
+	if (target->hand_size == 0){
+		printf("\n");
+		return; 
+	}
+for (int i = 0; i < target->hand_size; i++){
+	printf(" %s%s%c",temp->top.rank[1], temp->top.rank[0], temp->top.suit );
 }
-
+}
 void book(struct player* target)
 {
+	if (target->book == NULL || target->book[0] == '\0' || target == NULL){
+		printf("\n"); 
+		return;
+	}
+	for(int i = 0; i < 7 && target->book[i] != '\0'; i ++){
+		printf("%s ", target->book[i]); 
+	}
+	printf("\n"); 
 }
 
 void DispResult(){}
-
